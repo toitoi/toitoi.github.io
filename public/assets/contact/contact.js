14b3010412844a40092dd3221809f6dd83576ceb
SK. Ng <seekiatng@gmail.com>
2020-05-10 12:23:46 +0800

    remove the mailgun API.

--- public/assets/contact/contact.js
+++ public/assets/contact/contact.js
@@ -3,11 +3,6 @@
  * @version 1.0.0
  */
 
-$( document ).ready(function() {
-    
-});
-
-
 //check the form validation
 $("#contactForm").validator().on('submit', function (event) {
 
@@ -26,40 +21,18 @@ function submitForm(){
     var subject = $("#inputSubject").val();
     var message = $("#inputMessage").val();
 
-    const mg = window.mailgun.client({
-        username: 'api',
-        key: 'e5641726cdecfe30e7f2972af7ef72f4-0afbfc6c-c2ac2862',
-        public_key: 'pubkey-dcb09b1c95d74e3fc99903ecf3bb8da9'
-      });
-    //process remote data
-    mg.messages.create('toitoi.github.io', {
-        from: email,
-        to: ["davinci.softwares@gmail.com"],
-        subject: subject,
-        text: "name=" + name + "&email=" + email+ "&phone=" + phone +"&subject=" + subject + "&message=" + message,
-        html: ""
-      })
-      .then(msg => {
-          console.log(msg)
-          submitMSG('Message has been sent');
-        }) // logs response data
-      .catch(err => {
-          console.log(err)
-          submitMSG(data);
-        }); // logs any error
-
-    // $.ajax({
-    //     type: "POST",
-    //     url: "https://api.mailgun.net/v3/sandbox0ec74e800efd43a7b514927364f46d3e.mailgun.org/messages",
-    //     data: "name=" + name + "&email=" + email+ "&phone=" + phone +"&subject=" + subject + "&message=" + message,
-    //     success : function(data){
-    //         if (data == "success"){
-    //             submitMSG('Message has been sent');
-    //         } else {
-    //             submitMSG(data);
-    //         }
-    //     }
-    // });
+    $.ajax({
+        type: "POST",
+        url: "https://api.mailgun.net/v3/sandbox0ec74e800efd43a7b514927364f46d3e.mailgun.org/messages",
+        data: "name=" + name + "&email=" + email+ "&phone=" + phone +"&subject=" + subject + "&message=" + message,
+        success : function(data){
+            if (data == "success"){
+                submitMSG('Message has been sent');
+            } else {
+                submitMSG(data);
+            }
+        }
+    });
 }
 
 //submit message
